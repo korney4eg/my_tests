27c678b11e72c313b7f1196e5dca20671d657bf8
Aliaksei Karneyeu <aliaksei.karneyeu@grandparade.co.uk>
2018-08-19 01:44:40 +0200

    update tests

--- test.py
+++ test.py
@@ -2,20 +2,23 @@ import requests
 import random
 import string
 from datetime import date, datetime, timedelta
+
+server = 'app-lb-355220616.eu-west-1.elb.amazonaws.com'
+
 for i in range(1,5):
     user_name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
     print('Testing with unexisting user: ' +user_name)
     # checking unexisting user
-    r_get = requests.get('http://localhost:8080/hello/'+user_name)
+    r_get = requests.get('http://' + server + '/hello/'+user_name)
     print ('got response code: '+str(r_get.status_code))
     if r_get.status_code != 400:
         raise('User already exists')
     birth_day = str(datetime.today().date())
     print('Now lets add this user that has birhtday today ... '+birth_day)
-    r_put = requests.put('http://localhost:8080/hello/'+user_name, json= {'dateOfBirth':birth_day})
+    r_put = requests.put('http://' + server + '/hello/'+user_name, json= {'dateOfBirth':birth_day})
     print ('got response code: '+str(r_put.status_code))
     print('Check if user exists now ...')
-    r_get = requests.get('http://localhost:8080/hello/'+user_name)
+    r_get = requests.get('http://' + server + '/hello/'+user_name)
     print ('got response code: '+str(r_get.status_code))
     if r_get.status_code != 200:
         raise('User doesnt exists')
@@ -25,10 +28,10 @@ for i in range(1,5):
     print('Done ...')
     print('Will update birthday to 10   days before today')
     birth_day =  str((datetime.today() - timedelta(days=10)).date())
-    r_put = requests.put('http://localhost:8080/hello/'+user_name, json= {'dateOfBirth':birth_day})
+    r_put = requests.put('http://' + server + '/hello/'+user_name, json= {'dateOfBirth':birth_day})
     print ('got response code: '+str(r_put.status_code))
     print('Check if user date changed')
-    r_get = requests.get('http://localhost:8080/hello/'+user_name)
+    r_get = requests.get('http://' + server + '/hello/'+user_name)
     print ('got response code: '+str(r_get.status_code))
     if r_get.status_code != 200:
         raise('User doesnt exists')
